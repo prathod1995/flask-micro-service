736ac4a2fa0b1443fe5c12fdb6277bd8b8813449
prathod1995 <prathod@deqode.com>
2021-09-25 16:15:08 +0530

    First commit

---
+++ website/index.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>My E-com Shop</title>
+		</head>
+
+		<body>
+			<h1>Welcome to my Shop</h1>
+			<ul>
+				<?php
+				$json = file_get_contents('http://product-service');
+				$obj = json_decode($json);
+
+				$products = $obj->products;
+				foreach ($products as $product) {
+				    echo "<li>$product</li>";
+			        }
+				?>
+				</ul>
+				</body>
+</html>
+
+
